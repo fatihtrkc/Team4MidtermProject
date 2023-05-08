0126927a9c3d4f5f1e20bd510cf2d00c780ab0ff
ahmetgokalpg@gmail.com <ahmetgokalpg@gmail.com>
2023-05-08 20:23:19 +0300

    Added deneme

--- EntitiyLayer/Entities/AddedFood.cs
+++ EntitiyLayer/Entities/AddedFood.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EntityLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,18 @@ using System.Threading.Tasks;
 
 namespace EntitiyLayer.Entities
 {
-    internal class AddedFood
+    public class AddedFood
     {
+        public int AddedFoodId { get; set; }
+        public decimal CaloryPerUnit { get; set; }
+        public decimal TotalCalory { get; set; }
+        public DateTime CreationDate { get; set; }
+        public double TargetCaloryPerDay { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int FoodId { get; set; }
+        public Food Food { get; set; }
+        public int MealId { get; set; }
+        public Meal Meal { get; set; }
     }
 }
