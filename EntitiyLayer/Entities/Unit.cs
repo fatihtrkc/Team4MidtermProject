fa161d921bc1e6c3294e607069324be0853ac47b
fatihtrkc <fatih_trkci96@hotmail.com>
2023-05-08 22:18:27 +0300

    lifestyle

--- EntitiyLayer/Entities/Unit.cs
+++ EntitiyLayer/Entities/Unit.cs
@@ -2,6 +2,7 @@
 using EntityLayer.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,9 @@ namespace EntityLayer.Entities
 {
     public class Unit
     {
-        public int Id { get; set; }
+        [Key]
         public UnitType Name { get; set; }
+        public string Description { get; set; }
         public ICollection<Food> Foods { get; set; }
     }
 }
