bfe042d268180f8acaf15218e29fde6388df63fe
Mustafa Enes Karababa <nskarababa@gmail.com>
2023-05-09 12:11:27 +0300

    LifeStyleBL and MealBL Classes updated

--- BusinessLayer/EntitiesBL/LifeStyleBL.cs
+++ BusinessLayer/EntitiesBL/LifeStyleBL.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Interfaces;
+using DataAccessLayer.Context;
 using EntitiyLayer.Entities;
 using System;
 using System.Collections.Generic;
@@ -10,29 +11,37 @@ namespace BusinessLayer.EntitiesBL
 {
     public class LifeStyleBL : ICrud<LifeStyle>
     {
+        public LifeStyleBL()
+        {
+            db = new Team4Context();
+        }
+        Team4Context db;
         public bool Add(LifeStyle entity)
         {
-            throw new NotImplementedException();
+            db.LifeStyles.Add(entity);
+            return db.SaveChanges() > 0;
         }
 
         public bool Delete(int entityId)
         {
-            throw new NotImplementedException();
+            db.LifeStyles.Remove(Find(entityId));
+            return db.SaveChanges() > 0;
         }
 
         public LifeStyle Find(int entityId)
         {
-            throw new NotImplementedException();
+            return db.LifeStyles.Find(entityId);
         }
 
         public List<LifeStyle> GetAll()
         {
-            throw new NotImplementedException();
+            return db.LifeStyles.ToList();
         }
 
         public bool Update(LifeStyle entity)
         {
-            throw new NotImplementedException();
+            db.LifeStyles.Update(entity);
+            return db.SaveChanges() > 0;
         }
     }
 }
