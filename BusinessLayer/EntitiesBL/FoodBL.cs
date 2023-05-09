25d20084e73d7b54757c5f06b2c333ae2a3f197a
Mustafa Enes Karababa <nskarababa@gmail.com>
2023-05-09 11:31:59 +0300

    BL Class Added

---
+++ BusinessLayer/EntitiesBL/FoodBL.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Interfaces;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.EntitiesBL
+{
+    public class FoodBL : ICrud<Food>
+    {
+        public bool Add(Food entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Food Find(int entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Food> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Food entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
