72b77f5724d03e6a76614f7542b1d13152b397fb
fatihtrkc <fatih_trkci96@hotmail.com>
2023-05-09 11:48:23 +0300

    meal unitbl ler eklendi

---
+++ BusinessLayer/EntitiesBL/UnitBL.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Interfaces;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.EntitiesBL
+{
+    public class UnitBL : ICrud<Unit>
+    {
+        public bool Add(Unit entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Unit Find(int entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Unit> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Unit entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
