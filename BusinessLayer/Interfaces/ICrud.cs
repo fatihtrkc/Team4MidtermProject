4f714f53ab56c4226594b987bde726ab1a29f18b
fatihtrkc <fatih_trkci96@hotmail.com>
2023-05-09 11:26:11 +0300

    interface eklendi

---
+++ BusinessLayer/Interfaces/ICrud.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Interfaces
+{
+    internal interface ICrud<T> where T : class
+    {
+        public List<T> GetAll();
+        public bool Add(T entity);
+        public bool Delete(int entityId);
+        public bool Update(T entity);
+        public T Find(int entityId);
+    }
+}
