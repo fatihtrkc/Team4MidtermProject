0ff124f27f8f341fdd695c3eeb6932efb9d3db23
ahmetgokalpg@gmail.com <ahmetgokalpg@gmail.com>
2023-05-09 11:42:40 +0300

    yenidenCategory

--- BusinessLayer/EntitiesBL/CategoryBL.cs
+++ BusinessLayer/EntitiesBL/CategoryBL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Interfaces;
+using EntityLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.EntitiesBL
 {
-    internal class CategoryBL
+    public class CategoryBL : ICrud<Category>
     {
+        public bool Add(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category Find(int entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
