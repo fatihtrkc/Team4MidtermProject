3145d081f9049ffb5ee773ceb783d8ee310a5198
fatihtrkc <fatih_trkci96@hotmail.com>
2023-05-09 18:31:32 +0300

    new

--- DataAccessLayer/Configuration/UserCfg.cs
+++ DataAccessLayer/Configuration/UserCfg.cs
@@ -14,17 +14,19 @@ namespace DataAccessLayer.Configuration
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
-            builder.HasIndex(b => b.Email).IsUnique();
             builder.Property(b => b.Email).HasMaxLength(50);
+            builder.HasIndex(b => b.Email).IsUnique();
             builder.Property(b => b.Name).HasMaxLength(20);
             builder.Property(b => b.Surname).HasMaxLength(20);
             builder.Property(b => b.SpecificQuestion).HasMaxLength(100);
             builder.Property(b => b.QuestionAnswer).HasMaxLength(20);
+            builder.Property(b => b.ActivePassiveSituation).HasDefaultValue(true);
+            builder.Property(b => b.CreationDate).HasDefaultValue(DateTime.Now);
 
             builder.HasData
                 (
-                    new User() { Id = 1, Name = "Ahmet", Surname = "Yılmaz", Email = "ahmetyilmaz@yilmaz.com", Password = "AaBb12.,", Gender = Gender.Male, BirthDate = Convert.ToDateTime(2000 / 01 / 01), Height = 1.82, Weight = 103, SpecificQuestion = "En Sevdiğin Programlama Dili?", QuestionAnswer = "C#", LifeStyleId = LifeStyleType.Low, ActivePassiveSituation = true, CreationDate = DateTime.Now },
-                    new User() { Id = 2, Name = "Ayşe", Surname = "Sevgi", Email = "ayşesevgi@sevgi.com", Password = "AaBb12.,", Gender = Gender.Female, BirthDate = Convert.ToDateTime(1990 / 01 / 01), Height = 1.55, Weight = 55, SpecificQuestion = "En Sevdiğin Programlama Dili?", QuestionAnswer = "Java", LifeStyleId = LifeStyleType.Middle, ActivePassiveSituation = true, CreationDate = DateTime.Now }
+                    new User() { Id = 1, Name = "Ahmet", Surname = "Yılmaz", Email = "ahmetyilmaz@yilmaz.com", Password = "AaBb12.,", Gender = Gender.Male, BirthDate = Convert.ToDateTime(2000 / 01 / 01), Height = 1.82, Weight = 103, SpecificQuestion = "En Sevdiğin Programlama Dili?", QuestionAnswer = "C#", LifeStyleId = LifeStyleType.Low },
+                    new User() { Id = 2, Name = "Ayşe", Surname = "Sevgi", Email = "ayşesevgi@sevgi.com", Password = "AaBb12.,", Gender = Gender.Female, BirthDate = Convert.ToDateTime(1990 / 01 / 01), Height = 1.55, Weight = 55, SpecificQuestion = "En Sevdiğin Programlama Dili?", QuestionAnswer = "Java", LifeStyleId = LifeStyleType.Middle }
                 );
         }
     }
