ec8b8c5d1d84caa6546fa3b08801cdd5b1673971
ahmetgokalpg@gmail.com <ahmetgokalpg@gmail.com>
2023-05-11 20:47:47 +0300

    yeniseyler

--- UI_Layer/FoodStatisticsForm.cs
+++ UI_Layer/FoodStatisticsForm.cs
@@ -28,11 +28,12 @@ namespace UI_Layer
         private void FoodStatisticsForm_Load(object sender, EventArgs e)
         {
             FillList();
-            //FillListTopFood();
+            FillListTopFood();
         }
 
         private void FillList()
         {
+            lviewFoods.Items.Clear();
             List<Food> foods = dbbl.FoodBL.GetAll();
 
             foreach (Food item in foods)
@@ -65,22 +66,29 @@ namespace UI_Layer
             }
         }
 
-        //private void FillListTopFood()
-        //{
-        //    lviewTop.Items.Clear();
-        //    var list = dbbl.AddedFoodBL.GetAllByUserId(userId);
-        //    foreach (var item in list)
-        //    {
-        //        ListViewItem lvi = new ListViewItem();
+        private void FillListTopFood()
+        {
+            lviewTop.Items.Clear();
+            List<Food> list = dbbl.FoodBL.GetByText(txtSearch.Text);
+            foreach (Food item in list)
+            {
+                int kahvalti = dbbl.AddedFoodBL.GetCountFood(userId, item.Id, MealType.Breakfast);
+                int oglen = dbbl.AddedFoodBL.GetCountFood(userId, item.Id, MealType.Lunch);
+                int aksam = dbbl.AddedFoodBL.GetCountFood(userId, item.Id, MealType.Dinner);
+                int ara = dbbl.AddedFoodBL.GetCountFood(userId, item.Id, MealType.Snack);
+                int toplam = kahvalti + oglen + aksam + ara;
+                if (toplam > 0)
+                {
+                    ListViewItem lvi = new ListViewItem();
+                    lvi.Text = item.Name;
+                    lvi.SubItems.Add(toplam.ToString());
 
-        //        lvi.Text = item.FoodId.ToString();
-        //        lvi.SubItems.Add(dbbl.AddedFoodBL.GetCountFoodTop(userId, item.FoodId).ToString());
-        //        lviewTop.Items.Add(lvi);
+                    lviewTop.Items.Add(lvi);
+                }
+
+            }
 
 
-        //    }
-
-
-        //}
+        }
     }
 }
